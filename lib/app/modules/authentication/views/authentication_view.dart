16d2b271260ab5949f1975b51ccd261313781eee
Rijuth Menon <rijuthm@gmail.com>
2022-05-27 19:08:06 +0530

    init commit with getx pattern

---
+++ lib/app/modules/authentication/views/authentication_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/authentication_controller.dart';
+
+class AuthenticationView extends GetView<AuthenticationController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AuthenticationView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'AuthenticationView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
