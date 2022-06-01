d95cf2a3c434abc95bd78341b7315a3d8ef2eb80
Rijuth Menon <rijuthm@gmail.com>
2022-06-01 18:52:16 +0530

    created addPage

--- lib/app/modules/home/views/add_view.dart
+++ lib/app/modules/home/views/add_view.dart
@@ -1,32 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wekeep/app/data/models/product_model.dart';
 import 'package:wekeep/app/global_widgets/appBar.dart';
+import '../controllers/home_controller.dart';
 
-class AddView extends StatelessWidget {
-  const AddView({Key? key}) : super(key: key);
-
+class AddView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-
     return Scaffold(
       appBar: appBar,
       body: Column(children: [
         Form(
-          key: _formKey,
-          child: Column(children: [
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Name is not valid!';
-                } else {
-                  return null;
-                }
-                ;
-              },
-            )
-          ]),
+          key: controller.formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(children: [
+              Center(
+                child: Text('Enter product details'),
+              ),
+              TextFormField(
+                controller: controller.nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Name is not valid!';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              TextFormField(
+                controller: controller.nameController,
+                decoration: const InputDecoration(labelText: 'Warranty Months'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Warranty Months is not valid!';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              TextFormField(
+                controller: controller.nameController,
+                decoration: const InputDecoration(labelText: 'Category'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Category is not valid!';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              TextFormField(
+                controller: controller.nameController,
+                decoration: const InputDecoration(labelText: 'Notes'),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Notes is not valid!';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    final productModel = ProductModel(
+                        name: controller.nameController.text,
+                        category: controller.categoryController.text,
+                        warrantyMonths:
+                            controller.warrantyMonthsController.text,
+                        notes: controller.notesController.text);
+                  },
+                  child: Text('Add'))
+            ]),
+          ),
         )
       ]),
     );
