fab2b515a38b51d485263cea5df95c2fd986d7b6
Rijuth Menon <rijuthm@gmail.com>
2022-06-01 15:56:49 +0530

    added basic product model

---
+++ lib/app/data/models/product_model.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductModel {
+  String? productId;
+  String? notes;
+  late String name;
+  late String category;
+  late int warrantyMonths;
+
+  ProductModel(
+      {required this.name, required category, required warrantyMonths, notes});
+
+  /// Constructor to init variables from Firebase
+  ProductModel.fromDocumentSnapshot(
+      {required DocumentSnapshot documentSnapshot}) {
+    productId = documentSnapshot.id;
+    name = documentSnapshot['name'];
+    category = documentSnapshot['category'];
+    warrantyMonths = documentSnapshot['warrantyMonths'];
+    notes = documentSnapshot['notes'];
+  }
+}
