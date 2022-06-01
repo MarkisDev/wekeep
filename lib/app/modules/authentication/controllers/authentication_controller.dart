bda31599f0d0b71b24eaae8933c0496e908c8b71
Rijuth Menon <rijuthm@gmail.com>
2022-06-02 01:41:04 +0530

    using auth as const -> will move to DI using AuthController

--- lib/app/modules/authentication/controllers/authentication_controller.dart
+++ lib/app/modules/authentication/controllers/authentication_controller.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:wekeep/app/core/values/constants.dart';
 import 'package:wekeep/app/data/models/user_models.dart';
 import 'package:wekeep/app/data/providers/firestore_provider.dart';
 
 class AuthenticationController extends GetxController {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = GoogleSignIn();
-  final count = 0.obs;
+  final FirebaseAuth _auth = auth;
+  final GoogleSignIn _googleSignIn = googleSignIn;
   @override
   void onInit() {
     super.onInit();
@@ -25,7 +25,7 @@ class AuthenticationController extends GetxController {
   loginWithGoogle() async {
     try {
       final GoogleSignInAccount? googleSignInAccount =
-          await GoogleSignIn().signIn();
+          await _googleSignIn.signIn();
       final GoogleSignInAuthentication? googleSignInAuthentication =
           await googleSignInAccount?.authentication;
       final AuthCredential credential = GoogleAuthProvider.credential(
@@ -40,8 +40,8 @@ class AuthenticationController extends GetxController {
           email: googleSignInAccount.email,
           id: googleSignInAccount.id,
           photoUrl: googleSignInAccount.photoUrl.toString());
-      await FirestoreDb.addUser(userModel);
-      await Get.toNamed('/home', arguments: googleSignInAccount);
+      await FirestoreDb.addUser(userModel, _auth.currentUser!.uid);
+      await Get.toNamed('/home', arguments: _auth.currentUser!);
       return;
     } catch (e) {
       throw (e);
