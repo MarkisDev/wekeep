025aa9b0e8fba6c57b2b5a8cb77272319c0266ac
Rijuth Menon <rijuthm@gmail.com>
2022-06-02 01:37:58 +0530

    added auth as const for temp

---
+++ lib/app/core/values/constants.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+//-----FIREBASE AUTH CONSTANTS
+final FirebaseAuth auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
