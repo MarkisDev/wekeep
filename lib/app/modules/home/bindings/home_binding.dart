8d2faa38169f6e406e3c45df6641343969fc3260
Rijuth Menon <rijuthm@gmail.com>
2022-06-02 17:27:12 +0530

    added addController as dependency

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,13 +1,19 @@
 import 'package:get/get.dart';
+
 import 'package:wekeep/app/modules/authentication/controllers/authentication_controller.dart';
+import 'package:wekeep/app/modules/home/controllers/add_controller.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(() => HomeController(), fenix: true);
-    Get.lazyPut<AuthenticationController>(() => AuthenticationController(),
-        fenix: true);
+    Get.lazyPut<AddController>(
+      () => AddController(),
+    );
+    Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<AuthenticationController>(
+      () => AuthenticationController(),
+    );
   }
 }
