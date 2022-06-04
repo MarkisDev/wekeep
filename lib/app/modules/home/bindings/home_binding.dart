454f082f0f7756339578363c0793667ff78ff666
Rijuth Menon <rijuthm@gmail.com>
2022-06-05 01:07:53 +0530

    added catrgory controller

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -2,12 +2,16 @@ import 'package:get/get.dart';
 
 import 'package:wekeep/app/modules/authentication/controllers/authentication_controller.dart';
 import 'package:wekeep/app/modules/home/controllers/add_controller.dart';
+import 'package:wekeep/app/modules/home/controllers/category_controller.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<CategoryController>(
+      () => CategoryController(),
+    );
     Get.lazyPut<AddController>(
       () => AddController(),
     );
