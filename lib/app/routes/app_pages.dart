13b4efbd27d4c4edc12bff8253ebe647df526e3d
Rijuth Menon <rijuthm@gmail.com>
2022-06-05 19:16:55 +0530

    added new dependencies

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,8 @@ import '../modules/authentication/bindings/authentication_binding.dart';
 import '../modules/authentication/views/authentication_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/main_view.dart';
+import '../modules/service/bindings/service_binding.dart';
+import '../modules/service/views/service_view.dart';
 
 part 'app_routes.dart';
 
@@ -22,6 +24,11 @@ class AppPages {
       name: _Paths.AUTHENTICATION,
       page: () => AuthenticationView(),
       binding: AuthenticationBinding(),
+    ),
+    GetPage(
+      name: _Paths.SERVICE,
+      page: () => ServiceView(),
+      binding: ServiceBinding(),
     ),
   ];
 }
