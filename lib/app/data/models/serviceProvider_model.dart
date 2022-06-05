63011fcfc8f15ce9d282270e01e74660701ed803
Rijuth Menon <rijuthm@gmail.com>
2022-06-05 19:13:52 +0530

    added serviceProvider model

---
+++ lib/app/data/models/serviceProvider_model.dart
@@ -0,0 +1,7 @@
+class ServiceProvider {
+  late String name;
+  late String howFar;
+  late String imgUrl;
+  ServiceProvider(
+      {required this.name, required this.howFar, required this.imgUrl});
+}
