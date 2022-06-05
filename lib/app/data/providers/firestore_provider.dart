14f8ef298917475b9d96d5b8e7140aadb4583f44
Rijuth Menon <rijuthm@gmail.com>
2022-06-05 11:33:53 +0530

    added category CRUD functions

--- lib/app/data/providers/firestore_provider.dart
+++ lib/app/data/providers/firestore_provider.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:wekeep/app/data/models/category_model.dart';
 import 'package:wekeep/app/data/models/product_model.dart';
 import 'package:wekeep/app/data/models/user_models.dart';
 
@@ -55,5 +56,43 @@ class FirestoreDb {
       'id': userModel.id,
       'photoUrl': userModel.photoUrl,
     });
+  }
+
+//--------------Category Related Functions
+  static Stream<List<CategoryModel>> categoryStream(String uid) {
+    return _firebaseFirestore
+        .collection('users')
+        .doc(uid)
+        .collection('categories')
+        .snapshots()
+        .map((QuerySnapshot query) {
+      List<CategoryModel> products = [];
+      for (var product in query.docs) {
+        final productModel =
+            CategoryModel.fromDocumentSnapshot(documentSnapshot: product);
+        products.add(productModel);
+      }
+      return products;
+    });
+  }
+
+  static addCategory(CategoryModel categoryModel, String uid) async {
+    await _firebaseFirestore
+        .collection('users')
+        .doc(uid)
+        .collection('categories')
+        .add({
+      'name': categoryModel.name,
+      'color': categoryModel.color,
+    });
+  }
+
+  static deleteCategory(String documentId, String uid) {
+    _firebaseFirestore
+        .collection('users')
+        .doc(uid)
+        .collection('categories')
+        .doc(documentId)
+        .delete();
   }
 }
