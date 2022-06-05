4e7876ed0b841ecdb75703988ee522c664dc19be
Rijuth Menon <rijuthm@gmail.com>
2022-06-05 11:34:40 +0530

    binded stream for categories and helper functions

--- lib/app/modules/home/controllers/category_controller.dart
+++ lib/app/modules/home/controllers/category_controller.dart
@@ -1,7 +1,17 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:wekeep/app/data/models/category_model.dart';
+import 'package:wekeep/app/data/providers/firestore_provider.dart';
+import 'package:wekeep/app/modules/authentication/controllers/authentication_controller.dart';
 
 class CategoryController extends GetxController {
+  final categoryList = <CategoryModel>[].obs;
+  final pickedColor = Color(0xffffffff).obs;
+  final pickedCategoryName = ''.obs;
+  var nameController = TextEditingController();
+
+  List<CategoryModel> get categories => categoryList.value;
+
   Color? getColorFromHex(String? hexColor) {
     hexColor = hexColor?.replaceAll("#", "");
     if (hexColor?.length == 6) {
@@ -13,10 +23,24 @@ class CategoryController extends GetxController {
     }
   }
 
+  List<Map<String, dynamic>> getCategories() {
+    List<Map<String, dynamic>> newMap = [];
+    for (var item in categoryList) {
+      newMap.add({'value': item.name, 'label': item.name});
+    }
+    return newMap;
+  }
+
+  void changeColor(Color color) {
+    pickedColor.value = color;
+  }
+
   final count = 0.obs;
   @override
   void onInit() {
-    super.onInit();
+    final authenticationController = Get.find<AuthenticationController>();
+    categoryList.bindStream(FirestoreDb.categoryStream(
+        authenticationController.auth.currentUser!.uid));
   }
 
   @override
@@ -26,5 +50,4 @@ class CategoryController extends GetxController {
 
   @override
   void onClose() {}
-  void increment() => count.value++;
 }
