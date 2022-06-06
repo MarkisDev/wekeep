0997472dba8a0774e1c9a4e1963bced5a93448c8
Rijuth Menon <rijuthm@gmail.com>
2022-06-06 22:02:27 +0530

    added product repository

---
+++ lib/app/data/repositories/product_repository.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:wekeep/app/data/models/product_model.dart';
+import 'package:wekeep/app/data/providers/firestorage_provider.dart';
+import 'package:wekeep/app/data/providers/firestore_provider.dart';
+
+class ProductRepository {
+  addProduct(ProductModel productModel, String uid) async {
+    await FirestoreDb.addProduct(productModel, uid);
+  }
+
+  productStream(String uid) {
+    var productSnapshots = FirestoreDb.productStream(uid);
+    return productSnapshots.map((QuerySnapshot query) {
+      List<ProductModel> products = [];
+      for (var product in query.docs) {
+        final productModel =
+            ProductModel.fromDocumentSnapshot(documentSnapshot: product);
+        products.add(productModel);
+      }
+      return products;
+    });
+  }
+
+  deleteProduct(String documentId, String uid) async {
+    await FirestoreDb.deleteProduct(documentId, uid);
+  }
+}
