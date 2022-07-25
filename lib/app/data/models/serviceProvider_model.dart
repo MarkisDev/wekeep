e3d2bfc133695f6e25c9d26ec02f19f97086dbd8
Rijuth Menon <rijuthm@gmail.com>
2022-07-25 23:01:59 +0530

    added coords to model

--- lib/app/data/models/serviceProvider_model.dart
+++ lib/app/data/models/serviceProvider_model.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ServiceProvider {
   late String name;
   late double howFar;
   late String imgUrl;
+  late GeoPoint coords;
   ServiceProvider(
-      {required this.name, required this.howFar, required this.imgUrl});
+      {required this.name,
+      required this.howFar,
+      required this.imgUrl,
+      required this.coords});
 }
