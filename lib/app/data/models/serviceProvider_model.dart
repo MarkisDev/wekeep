9d637be259c583b8612184382707ff9c6107ac3a
Rijuth Menon <rijuthm@gmail.com>
2022-07-25 23:27:25 +0530

    added uid field for shop doc

--- lib/app/data/models/serviceProvider_model.dart
+++ lib/app/data/models/serviceProvider_model.dart
@@ -1,12 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ServiceProvider {
+  late String uid;
   late String name;
   late double howFar;
   late String imgUrl;
   late GeoPoint coords;
   ServiceProvider(
-      {required this.name,
+      {required this.uid,
+      required this.name,
       required this.howFar,
       required this.imgUrl,
       required this.coords});
