4354880aa39f223524c10153ba37807f7b146165
Rijuth Menon <rijuthm@gmail.com>
2022-07-25 23:28:23 +0530

    taking service model in request view to update db with new request

--- lib/app/modules/service/views/request_view.dart
+++ lib/app/modules/service/views/request_view.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:wekeep/app/data/models/request_model.dart';
+import 'package:wekeep/app/data/models/serviceProvider_model.dart';
 import 'package:wekeep/app/global_widgets/appBar.dart';
 import 'package:wekeep/app/modules/service/controllers/service_controller.dart';
 
 class RequestView extends GetView<ServiceController> {
   @override
   Widget build(BuildContext context) {
+    ServiceProvider serviceModel = Get.arguments;
+
     return Scaffold(
       appBar: appBar,
       body: Center(
@@ -36,18 +40,18 @@ class RequestView extends GetView<ServiceController> {
                         }
                       },
                     ),
-                    TextFormField(
-                      controller: controller.emailController,
-                      decoration: const InputDecoration(labelText: 'Email'),
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Email is not valid!';
-                        } else {
-                          return null;
-                        }
-                      },
-                    ),
+                    // TextFormField(
+                    //   controller: controller.emailController,
+                    //   decoration: const InputDecoration(labelText: 'Email'),
+                    //   autovalidateMode: AutovalidateMode.onUserInteraction,
+                    //   validator: (value) {
+                    //     if (value == null || value.isEmpty) {
+                    //       return 'Email is not valid!';
+                    //     } else {
+                    //       return null;
+                    //     }
+                    //   },
+                    // ),
                     TextFormField(
                       minLines: 3,
                       maxLines: 10,
@@ -65,7 +69,26 @@ class RequestView extends GetView<ServiceController> {
                     ),
                     ElevatedButton(
                         onPressed: () async {
-                          await controller.sendEmail();
+                          RequestModel requestModel = RequestModel(
+                              img:
+                                  'https://lh3.googleusercontent.com/a/AATXAJwn7u2KvmoKJnNbsWclKbLCJEqDcGhI-iCq0mU-9Q=s96-c',
+                              name: controller.nameController.text,
+                              desc: controller.messageController.text);
+                          controller.repository
+                              .addRequest(requestModel, serviceModel.uid);
+                          Get.snackbar(
+                            'Success!',
+                            'Sent request!',
+                            snackPosition: SnackPosition.BOTTOM,
+                            backgroundColor: Colors.green,
+                            borderRadius: 20,
+                            margin: EdgeInsets.all(15),
+                            colorText: Colors.white,
+                            duration: Duration(seconds: 4),
+                            isDismissible: true,
+                            dismissDirection: DismissDirection.horizontal,
+                            forwardAnimationCurve: Curves.easeOutBack,
+                          );
                           Get.offNamed('/service');
                         },
                         child: Text('Send'))
