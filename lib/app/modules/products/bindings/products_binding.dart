2968708f24ea378f66df7c52e762c1b3077e84ae
Rijuth Menon <rijuthm@gmail.com>
2022-07-25 19:32:16 +0530

    made products controller fenix

--- lib/app/modules/products/bindings/products_binding.dart
+++ lib/app/modules/products/bindings/products_binding.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
 import 'package:wekeep/app/data/repositories/product_repository.dart';
+import 'package:wekeep/app/data/repositories/service_repository.dart';
 import 'package:wekeep/app/modules/authentication/controllers/authentication_controller.dart';
+import 'package:wekeep/app/modules/service/controllers/service_controller.dart';
+import 'package:wekeep/app/modules/service/views/serviceDetails_view.dart';
 
 import '../controllers/products_controller.dart';
 
@@ -9,6 +12,7 @@ class ProductsBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<ProductsController>(
       () => ProductsController(repository: ProductRepository()),
+      fenix: true,
     );
   }
 }
