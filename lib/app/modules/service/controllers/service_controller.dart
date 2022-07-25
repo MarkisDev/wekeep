0aaa09908f6e618d31e9e820b028478e17e6eaa3
Rijuth Menon <rijuthm@gmail.com>
2022-07-25 19:34:32 +0530

    added location method in service view

--- lib/app/modules/service/controllers/service_controller.dart
+++ lib/app/modules/service/controllers/service_controller.dart
@@ -1,12 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:wekeep/app/data/models/serviceProvider_model.dart';
+import 'package:wekeep/app/data/repositories/service_repository.dart';
 
 class ServiceController extends GetxController {
+  final ServiceRepository repository;
+  ServiceController({required this.repository});
+
+  final geo = Geoflutterfire();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   var nameController = TextEditingController();
   var messageController = TextEditingController();
   var emailController = TextEditingController();
+  var shopsList = [].obs;
+  final center =
+      Geoflutterfire().point(latitude: 12.9633978, longitude: 77.58550823).obs;
+  get shops => shopsList.value;
 
   Future<void> sendEmail() async {
     await FlutterEmailSender.send(Email(
@@ -15,9 +28,40 @@ class ServiceController extends GetxController {
         recipients: ['rir20cs@cmrit.com']));
   }
 
+  getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    var x = await Geolocator.getCurrentPosition();
+    GeoFirePoint pos = geo.point(latitude: x.latitude, longitude: x.longitude);
+    return pos;
+  }
+
+  getShopList() {
+    return repository.getShops(center.value);
+  }
+
   @override
   void onInit() {
-    super.onInit();
+    shopsList.bindStream(repository.getShops(center.value));
   }
 
   @override
@@ -26,5 +70,7 @@ class ServiceController extends GetxController {
   }
 
   @override
-  void onClose() {}
+  void onClose() {
+    super.onClose();
+  }
 }
