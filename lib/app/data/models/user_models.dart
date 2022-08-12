3d08b340fd03edf897dba42eb85869cd63917952
Rijuth Menon <rijuthm@gmail.com>
2022-08-12 23:53:23 +0530

    added token to userModel

--- lib/app/data/models/user_models.dart
+++ lib/app/data/models/user_models.dart
@@ -1,14 +1,14 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class UserModel {
   late String name;
   late String email;
   late String id;
   late String photoUrl;
+  late String token;
 
   UserModel(
       {required this.name,
       required this.email,
       required this.id,
-      required this.photoUrl});
+      required this.photoUrl,
+      required this.token});
 }
