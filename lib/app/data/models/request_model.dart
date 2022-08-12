a18507813ecd1f693b54aaf67878d78462cc2835
Rijuth Menon <rijuthm@gmail.com>
2022-08-12 23:52:06 +0530

    added token and uid to requestModel

--- lib/app/data/models/request_model.dart
+++ lib/app/data/models/request_model.dart
@@ -4,6 +4,13 @@ class RequestModel {
   String img;
   String desc;
   String name;
+  String uid;
+  String token;
 
-  RequestModel({required this.img, required this.name, required this.desc});
+  RequestModel(
+      {required this.img,
+      required this.name,
+      required this.desc,
+      required this.uid,
+      required this.token});
 }
