7e7eebb9b947b20d56a72f65decf1564d4259563
Rijuth Menon <rijuthm@gmail.com>
2022-10-15 22:59:42 +0530

    added new colors

--- lib/app/ui/theme/color_theme.dart
+++ lib/app/ui/theme/color_theme.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 Color kprimaryColor = Color(0xff557CDF);
+Color ksecondaryColor = Color(0xff9C80EA);
+Color kthirdColor = Color(0xffC088F3);
+Color klightColor = Color(0xffF8F2F5);
