64e022c94cbebb8311da81d073dea396035884be
Rijuth Menon <rijuthm@gmail.com>
2022-10-15 16:47:42 +0530

    changed login screen to new look

--- lib/app/modules/authentication/views/authentication_view.dart
+++ lib/app/modules/authentication/views/authentication_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
+import 'package:wekeep/app/ui/theme/color_theme.dart';
 
 import '../controllers/authentication_controller.dart';
+import 'package:getwidget/getwidget.dart';
 
 class AuthenticationView extends StatelessWidget {
   AuthenticationView({Key? key}) : super(key: key);
@@ -10,27 +14,62 @@ class AuthenticationView extends StatelessWidget {
       Get.find<AuthenticationController>();
   @override
   Widget build(BuildContext context) {
+    var height = Get.height;
+    var width = Get.width;
     return Scaffold(
+      backgroundColor: kprimaryColor,
       appBar: AppBar(
-        title: Text('Login'),
+        elevation: 0,
+        backgroundColor: kprimaryColor,
+        centerTitle: true,
+        // title: Text('Login'),
       ),
       body: Padding(
         padding: EdgeInsets.all(12.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
+            Column(
+              children: [
+                Text(
+                  'WeKeep',
+                  style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text(
+                  'Quality service at your doorstep!',
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white),
+                ),
+              ],
+            ),
+            Center(
+              child: Container(
+                child: SvgPicture.asset('assets/images/login.svg'),
+                height: height * 0.4,
+                width: width * 0.5,
               ),
-              onPressed: () => controller.loginWithGoogle(),
-              child: Center(
-                  child: Text(
-                "Login with google",
-                style: TextStyle(color: Colors.white),
-              )),
-            )
+            ),
+            Column(
+              children: [
+                Text(
+                  "Welcome back! To get started, please login!",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white),
+                )
+              ],
+            ),
+            Center(
+              child: SignInButton(Buttons.Google,
+                  onPressed: () => controller.loginWithGoogle()),
+            ),
           ],
         ),
       ),
