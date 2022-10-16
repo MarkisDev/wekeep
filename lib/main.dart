5e7aa846fb01b452b7c6ad455302aea58168f003
Rijuth Menon <rijuthm@gmail.com>
2022-10-16 10:06:32 +0530

    added primary color to theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:wekeep/app/ui/theme/color_theme.dart';
 
 import 'app/routes/app_pages.dart';
 
@@ -10,6 +11,10 @@ void main() async {
   await Firebase.initializeApp();
   runApp(
     GetMaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: ThemeData()
+              .colorScheme
+              .copyWith(primary: kprimaryColor, secondary: ksecondaryColor)),
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
