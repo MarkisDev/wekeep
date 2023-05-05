b9aa5d867fef2a9553533223b5d5a1a8b60fb40e
Rijuth Menon <rijuthm@gmail.com>
2023-05-06 02:13:48 +0530

    storing oneSignal token in db during auth

--- lib/app/modules/authentication/controllers/authentication_controller.dart
+++ lib/app/modules/authentication/controllers/authentication_controller.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:wekeep/app/data/models/user_models.dart';
 import 'package:wekeep/app/data/providers/firestore_provider.dart';
 import 'package:wekeep/app/modules/authentication/views/authentication_view.dart';
@@ -11,6 +12,7 @@ class AuthenticationController extends GetxController {
   final FirebaseAuth auth = FirebaseAuth.instance;
   final GoogleSignIn googleSignIn = GoogleSignIn();
   final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  late final UserModel userModel;
   @override
   void onInit() {
     super.onInit();
@@ -36,18 +38,21 @@ class AuthenticationController extends GetxController {
         idToken: googleSignInAuthentication.idToken,
       );
       final authResult = await auth.signInWithCredential(credential);
-      final token = await firebaseMessaging.getToken();
+
+      final status = await OneSignal.shared.getDeviceState();
+      final String? token = status?.userId;
+
       // Firebase Auth User
       // final User? user = authResult.user;
 
-      UserModel userModel = UserModel(
+      userModel = UserModel(
           name: googleSignInAccount!.displayName.toString(),
           email: googleSignInAccount.email,
           id: googleSignInAccount.id,
           photoUrl: googleSignInAccount.photoUrl.toString(),
           token: token.toString());
       await FirestoreDb.addUser(userModel, auth.currentUser!.uid);
-      await Get.offNamed('/home', arguments: userModel);
+      await Get.toNamed('/home', arguments: userModel);
       return;
     } catch (e) {
       throw (e);
