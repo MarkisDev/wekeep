aa40b1afe1b753ca61a1ad273a26f6e540eab526
Rijuth Menon <rijuthm@gmail.com>
2023-05-06 02:11:06 +0530

    updated model to track warranty

--- lib/app/data/models/product_model.dart
+++ lib/app/data/models/product_model.dart
@@ -4,17 +4,23 @@ class ProductModel {
   String? productId;
   String? notes;
   String? receiptUrl;
+  String? productUrl;
+
   late String name;
   late String categoryId;
   late String categoryName;
   late int warrantyMonths;
-
+  late String purchaseDate;
+  late String warrantyDate;
   ProductModel(
       {required this.name,
       required this.categoryName,
       required this.categoryId,
+      required this.purchaseDate,
+      required this.warrantyDate,
       required this.warrantyMonths,
       this.receiptUrl,
+      this.productUrl,
       this.notes});
 
   /// Constructor to init variables from Firebase
@@ -25,7 +31,10 @@ class ProductModel {
     categoryId = documentSnapshot['categoryId'];
     categoryName = documentSnapshot['categoryName'];
     warrantyMonths = documentSnapshot['warrantyMonths'];
+    warrantyDate = documentSnapshot['warrantyDate'];
+    purchaseDate = documentSnapshot['purchaseDate'];
     notes = documentSnapshot['notes'];
     receiptUrl = documentSnapshot['receiptUrl'];
+    productUrl = documentSnapshot['productUrl'];
   }
 }
