330e49e0bcc925c71abc0936d8858ac785be7530
Rijuth Menon <rijuthm@gmail.com>
2023-05-06 02:19:26 +0530

    setting up oneSignal

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:wekeep/app/ui/theme/color_theme.dart';
 
 import 'app/routes/app_pages.dart';
@@ -9,6 +10,13 @@ import 'app/routes/app_pages.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  OneSignal.shared.setAppId(
+    "7401b412-9e81-4d6f-9efd-15cb8807415c",
+  );
+  OneSignal.shared.setRequiresUserPrivacyConsent(false);
+
   runApp(
     GetMaterialApp(
       theme: ThemeData().copyWith(
