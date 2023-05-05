c2cd0a87e1f8f58be7690ec7b934a0e4cd1fd3c5
Rijuth Menon <rijuthm@gmail.com>
2023-05-06 02:12:04 +0530

    updated model to fetch photoUrl

--- lib/app/data/models/request_model.dart
+++ lib/app/data/models/request_model.dart
@@ -6,11 +6,15 @@ class RequestModel {
   String name;
   String uid;
   String token;
+  String userName;
+  String userPhotoUrl;
 
   RequestModel(
       {required this.img,
       required this.name,
       required this.desc,
       required this.uid,
-      required this.token});
+      required this.token,
+      required this.userName,
+      required this.userPhotoUrl});
 }
