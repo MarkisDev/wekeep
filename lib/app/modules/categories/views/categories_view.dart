01e0409ba0596b3c48bda8cecb4fb9f53a60e6fc
Rijuth Menon <rijuthm@gmail.com>
2023-05-06 02:14:33 +0530

    better UI for categories

--- lib/app/modules/categories/views/categories_view.dart
+++ lib/app/modules/categories/views/categories_view.dart
@@ -30,33 +30,32 @@ class CategoriesView extends GetView<CategoriesController> {
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            "Welcome ${authenticationController.auth.currentUser!.displayName!.split(' ')[0].toString()}!",
-                            style: TextStyle(
-                                fontSize: 24,
-                                // fontWeight: FontWeight,
-                                color: Colors.white),
-                          ),
-                          GFAvatar(
-                            shape: GFAvatarShape.standard,
-                            backgroundImage: NetworkImage(
-                                authenticationController
-                                    .auth.currentUser!.photoURL
-                                    .toString()),
-                            size: 30,
-                          ),
-                        ],
+                      GFAvatar(
+                        shape: GFAvatarShape.standard,
+                        backgroundImage: NetworkImage(authenticationController
+                            .auth.currentUser!.photoURL
+                            .toString()),
+                        size: 30,
                       ),
                       Padding(
-                        padding: const EdgeInsets.only(bottom: 15.0),
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          "Welcome ${authenticationController.auth.currentUser!.displayName!.split(' ')[0].toString()}!",
+                          style: TextStyle(
+                              fontSize: 24,
+                              // fontWeight: FontWeight,
+                              color: Colors.white),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
                             GFButton(
-                              onPressed: () {},
+                              onPressed: () async {
+                                await authenticationController.logoutGoogle();
+                              },
                               icon: Icon(Icons.logout),
                               color: Colors.white,
                               text: ('Logout'),
@@ -325,105 +324,115 @@ class CategoriesView extends GetView<CategoriesController> {
   }
 }
 
-class categoryForm extends GetView<AuthenticationController> {
+dynamic categoryForm() {
   final CategoriesController categoriesController =
       Get.find<CategoriesController>();
-  @override
-  Widget build(BuildContext context) {
-    var height = Get.height;
-    var width = Get.width;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SingleChildScrollView(
-        child: Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                child: SvgPicture.asset('assets/images/addCategory.svg'),
-                height: height * 0.2,
-              ),
-              TextFormField(
-                controller: categoriesController.nameController,
-                decoration: InputDecoration(
-                  labelText: 'Name',
+  final AuthenticationController controller =
+      Get.find<AuthenticationController>();
+  var height = Get.height;
+  var width = Get.width;
+  return Get.defaultDialog(
+      title: '',
+      content: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  child: SvgPicture.asset('assets/images/addCategory.svg'),
+                  height: height * 0.2,
                 ),
-              ),
-              Obx(() => Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: GFButton(
-                        type: GFButtonType.outline2x,
-                        color: kprimaryColor,
-                        textStyle: TextStyle(
-                            fontSize: 16,
-                            color: kprimaryColor,
-                            fontWeight: FontWeight.w500),
-                        onPressed: () {
-                          Get.defaultDialog(
-                              content: Obx(() => ColorPicker(
-                                    pickerColor:
-                                        categoriesController.pickedColor.value,
-                                    onColorChanged:
-                                        categoriesController.changeColor,
-                                  )));
-                        },
-                        child: (categoriesController.categoryChanged == false)
-                            ? Expanded(child: Text('Choose color'))
-                            : Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Text('Choose color'),
-                                  GFAvatar(
-                                    backgroundColor:
-                                        categoriesController.pickedColor.value,
-                                    size: 14,
-                                  ),
-                                ],
-                              )),
-                  )),
-              ElevatedButton(
-                  onPressed: () async {
-                    if (categoriesController.nameController.text.isEmpty) {
-                      Get.snackbar(
-                        'Error!',
-                        'Enter a category name!',
-                        snackPosition: SnackPosition.BOTTOM,
-                        backgroundColor: Colors.red,
-                        borderRadius: 20,
-                        margin: EdgeInsets.all(15),
-                        colorText: Colors.white,
-                        duration: Duration(seconds: 4),
-                        isDismissible: true,
-                        dismissDirection: DismissDirection.horizontal,
-                        forwardAnimationCurve: Curves.easeOutBack,
-                      );
-                    } else {
-                      CategoryModel categoryModel = CategoryModel(
-                          '#${categoriesController.pickedColor.value.value.toRadixString(16)}',
-                          categoriesController.nameController.text);
-                      await categoriesController.repository.addCategory(
-                          categoryModel, controller.auth.currentUser!.uid);
-                      Get.snackbar(
-                        'Success!',
-                        'Added Category!',
-                        snackPosition: SnackPosition.BOTTOM,
-                        backgroundColor: Colors.green,
-                        borderRadius: 20,
-                        margin: EdgeInsets.all(15),
-                        colorText: Colors.white,
-                        duration: Duration(seconds: 4),
-                        isDismissible: true,
-                        dismissDirection: DismissDirection.horizontal,
-                        forwardAnimationCurve: Curves.easeOutBack,
-                      );
-                    }
-                  },
-                  child: Text('Add Category'))
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    'Enter the new category details!',
+                    style: TextStyle(fontSize: 21),
+                  ),
+                ),
+                TextFormField(
+                  controller: categoriesController.nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                  ),
+                ),
+                Obx(() => Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: GFButton(
+                          type: GFButtonType.outline2x,
+                          color: kprimaryColor,
+                          textStyle: TextStyle(
+                              fontSize: 16,
+                              color: kprimaryColor,
+                              fontWeight: FontWeight.w500),
+                          onPressed: () {
+                            Get.defaultDialog(
+                                content: Obx(() => ColorPicker(
+                                      pickerColor: categoriesController
+                                          .pickedColor.value,
+                                      onColorChanged:
+                                          categoriesController.changeColor,
+                                    )));
+                          },
+                          child: (categoriesController.categoryChanged == false)
+                              ? Text('Choose color')
+                              : Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text('Choose color'),
+                                    GFAvatar(
+                                      backgroundColor: categoriesController
+                                          .pickedColor.value,
+                                      size: 14,
+                                    ),
+                                  ],
+                                )),
+                    )),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (categoriesController.nameController.text.isEmpty) {
+                        Get.snackbar(
+                          'Error!',
+                          'Enter a category name!',
+                          snackPosition: SnackPosition.BOTTOM,
+                          backgroundColor: Colors.red,
+                          borderRadius: 20,
+                          margin: EdgeInsets.all(15),
+                          colorText: Colors.white,
+                          duration: Duration(seconds: 4),
+                          isDismissible: true,
+                          dismissDirection: DismissDirection.horizontal,
+                          forwardAnimationCurve: Curves.easeOutBack,
+                        );
+                      } else {
+                        CategoryModel categoryModel = CategoryModel(
+                            '#${categoriesController.pickedColor.value.value.toRadixString(16)}',
+                            categoriesController.nameController.text);
+                        await categoriesController.repository.addCategory(
+                            categoryModel, controller.auth.currentUser!.uid);
+                        Get.snackbar(
+                          'Success!',
+                          'Added Category!',
+                          snackPosition: SnackPosition.BOTTOM,
+                          backgroundColor: Colors.green,
+                          borderRadius: 20,
+                          margin: EdgeInsets.all(15),
+                          colorText: Colors.white,
+                          duration: Duration(seconds: 4),
+                          isDismissible: true,
+                          dismissDirection: DismissDirection.horizontal,
+                          forwardAnimationCurve: Curves.easeOutBack,
+                        );
+                        categoriesController.nameController.clear();
+                        categoriesController.categoryChanged.value = false;
+                      }
+                    },
+                    child: Text('Add Category'))
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+      ));
 }
